f69643375bd0bbe6db80c1146bd5791059d13d3f
Karla Mixco <119469711+kmixco@users.noreply.github.com>
2022-12-04 21:48:48 -0600

    Update RegistroAlumno.vb
    Correccion de botones

--- RegistroAlumno.vb
+++ RegistroAlumno.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Text.RegularExpressions
+Imports System.Text.RegularExpressions
 Public Class RegistroAlumno
     Dim alumno As New alumno
     Private Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles btnGenerar.Click
@@ -17,23 +17,6 @@ Public Class RegistroAlumno
 
     Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
 
-        'Validar correo
-        Dim regex As Regex = New Regex("^[^@\s]+@[^@\s]+\.[^@\s]+$")
-        Dim isvalid As Boolean = regex.IsMatch(txtCorreo.Text.Trim)
-
-        If Not isvalid Then
-            MsgBox("Debe ingresar un correo valido")
-        End If
-
-
-        'Validar DUI
-        Dim regexDUI As Regex = New Regex("^[^@\s]+-[^@\s]+$")
-        Dim isduivalid As Boolean = regexDUI.IsMatch(txtDui.Text.Trim)
-
-        If Not isduivalid Then
-            MsgBox("Debe ingresar un DUI valido, recuerde usar guiones")
-        End If
-
         alumno.datosAlumno(txtCodigo.Text,
                            txtNombre.Text,
                            txtApellido.Text,
@@ -43,7 +26,9 @@ Public Class RegistroAlumno
                            txtEdad.Text,
                            txtCorreo.Text)
 
-        VerDatos.Rows.Add(txtCodigo.Text,
+        If alumno.datosAceptados Then
+            MsgBox("Datos de " & alumno.nombreAlumno & " " & alumno.apellidoAlumno & " han sido registrados con éxito")
+            VerDatos.Rows.Add(txtCodigo.Text,
                          alumno.nombreAlumno,
                          alumno.apellidoAlumno,
                          alumno.generoAlumno,
@@ -51,10 +36,15 @@ Public Class RegistroAlumno
                          alumno.duiAlumno,
                          alumno.correoAlumno,
                          alumno.edadAlumno)
-
-        If alumno.datosAceptados Then
-            MsgBox("Datos de " & alumno.nombreAlumno & " " & alumno.apellidoAlumno & " han sido registrados con éxito")
-
+        Else
+            txtCodigo.Text = ""
+            txtNombre.Text = ""
+            txtApellido.Text = ""
+            txtGenero.Text = ""
+            txtDireccion.Text = ""
+            txtDui.Text = ""
+            txtEdad.Text = ""
+            txtCorreo.Text = ""
         End If
 
         txtCodigo.Text = ""
@@ -68,23 +58,4 @@ Public Class RegistroAlumno
 
     End Sub
 
-    Private Sub RegistroAlumno_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub lbRegistro_Click(sender As Object, e As EventArgs) Handles lbRegistro.Click
-
-    End Sub
-
-    Private Sub txtApellido_TextChanged(sender As Object, e As EventArgs) Handles txtApellido.TextChanged
-
-    End Sub
-
-    Private Sub txtCodigo_TextChanged(sender As Object, e As EventArgs) Handles txtCodigo.TextChanged
-
-    End Sub
-
-    Private Sub VerDatos_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles VerDatos.CellContentClick
-
-    End Sub
 End Class
